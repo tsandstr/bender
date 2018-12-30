5be397ef855a8cf297bfbe13f2349a60875c16f6
Theo Sandstrom <theo.j.sandstrom@gmail.com>
2018-12-29 22:56:04 -0500

    Changed license to GPL v3.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :description "The code for Bender, an imaginary FRC robot."
   :url "https://github.com/tsandstr/bender"
   :license {:name "GPLv3.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :repositories [["first" "http://first.wpi.edu/FRC/roborio/maven/development/"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [edu.wpi.first.wpilibj/wpilibj-java "2019.1.1-beta-4-17-g300eeb3"]
